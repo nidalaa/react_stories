a85d819dc0652b6bb6b16c6496507cb419816772
Emilia <e.andrzejewska@pilot.co>
2015-03-07 11:38:27 +0100

    #3 React components basics- fetching data from api

--- src/app.js
+++ src/app.js
@@ -1,19 +1,69 @@
 "use strict";
 
 var React = require('react');
+var $ = require('jquery');
 
-var SimpleComponent = React.createClass({
+var Story = React.createClass({
   render: function() {
     return (
+      <p>
+        <b> {this.props.title} </b>
+        <a href={this.props.url}> Read it! </a>
+        <span> score: {this.props.score} </span>
+      </p>
+    );
+  }
+});
+
+var StoriesCollection = React.createClass({
+  render: function() {
+    var storiesNodes = this.props.storiesData.map(function(story){
+      return (
+        <li>
+          <Story id={story.id} title={story.title} url={story.url} score={story.score}/>
+        </li>
+      );
+    });
+
+    return(
+      <ul>
+        {storiesNodes}
+      </ul>
+    );
+  }
+});
+
+
+var StoriesBox = React.createClass({
+  getInitialState: function(){
+    return {storiesData: []}
+  },
+
+  componentDidMount: function(){
+    $.ajax({
+      url: this.props.apiUrl,
+      dataType: 'json',
+      success: function(data) {
+        if (this.isMounted()) {
+          this.setState({storiesData: data});
+        }
+      }.bind(this)
+    });
+  },
+
+  render: function(){
+    return(
       <div>
-        Hello World! :):)
+        <h1>The newest amazing stories for you!</h1>
+        <h2>See them all:</h2>
+        <StoriesCollection storiesData={this.state.storiesData}/>
       </div>
     );
   }
 });
 
 document.addEventListener("DOMContentLoaded", function(){
-  React.render(<SimpleComponent/>, document.getElementById("content"));
+  React.render(<StoriesBox apiUrl="https://fierce-gorge-1132.herokuapp.com/stories" />, document.getElementById("content"));
 });
 
 
