5bfa8c8cfd874b332ece6d844511c83c12c4730c
Emilia <e.andrzejewska@pilot.co>
2015-03-07 16:13:35 +0100

    #4 adding basic styling with bootstrap and react inline styles

--- src/app.js
+++ src/app.js
@@ -1,15 +1,24 @@
 "use strict";
 
+global.jQuery = require('jquery');
+global.$ = require('jquery');
 var React = require('react');
-var $ = require('jquery');
+var Badge = require('react-bootstrap/lib/Badge');
+var Navbar = require('react-bootstrap/lib/Navbar');
+var Nav = require('react-bootstrap/lib/Nav');
+var NavItem = require('react-bootstrap/lib/NavItem');
+var ListGroup = require('react-bootstrap/lib/ListGroup');
+var ListGroupItem = require('react-bootstrap/lib/ListGroupItem');
+var PageHeader = require('react-bootstrap/lib/PageHeader');
+
 
 var Story = React.createClass({
   render: function() {
     return (
       <p>
+        <Badge>{this.props.score}</Badge>
         <b> {this.props.title} </b>
         <a href={this.props.url}> Read it! </a>
-        <span> score: {this.props.score} </span>
       </p>
     );
   }
@@ -19,24 +28,41 @@ var StoriesCollection = React.createClass({
   render: function() {
     var storiesNodes = this.props.storiesData.map(function(story){
       return (
-        <li>
+        <ListGroupItem>
           <Story id={story.id} title={story.title} url={story.url} score={story.score}/>
-        </li>
+        </ListGroupItem>
       );
     });
 
     return(
-      <ul>
+      <ListGroup>
         {storiesNodes}
-      </ul>
+      </ListGroup>
     );
   }
 });
 
 
+var StoriesMenu = React.createClass({
+  render: function(){
+    return(
+      <div>
+      <Navbar brand="React Stories">
+        <Nav>
+          <NavItem eventKey={1} href="#">Popular</NavItem>
+          <NavItem eventKey={2} href="#">Recent</NavItem>
+        </Nav>
+      </Navbar>
+      </div>
+    );
+  }
+});
+
+
+
 var StoriesBox = React.createClass({
   getInitialState: function(){
-    return {storiesData: []}
+    return {storiesData: [{"id": 1, "title": "Title", "url": "url", "score": "+2000"}]}
   },
 
   componentDidMount: function(){
@@ -52,11 +78,19 @@ var StoriesBox = React.createClass({
   },
 
   render: function(){
+    var wrapperStyle = {
+      width: '1100px',
+      margin: 'auto'
+    };
+
     return(
       <div>
-        <h1>The newest amazing stories for you!</h1>
-        <h2>See them all:</h2>
-        <StoriesCollection storiesData={this.state.storiesData}/>
+        <StoriesMenu/>
+
+        <div style={wrapperStyle}>
+          <PageHeader>The newest amazing stories for you! See them all!</PageHeader>
+          <StoriesCollection storiesData={this.state.storiesData}/>
+        </div>
       </div>
     );
   }
