ea44fa2793cc2899793f9c735c970f04ac3d449e
Emilia <e.andrzejewska@pilot.co>
2015-03-07 10:45:24 +0100

    #2 Simple hello world component in Ract

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+"use strict";
+
+var React = require('react');
+
+var SimpleComponent = React.createClass({
+  render: function() {
+    return (
+      <div>
+        Hello World! :):)
+      </div>
+    );
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+  React.render(<SimpleComponent/>, document.getElementById("content"));
+});
+
 
